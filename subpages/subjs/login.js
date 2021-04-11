09a1f197bdd48d46af94ee726e4795da2449b514
Radical06 <radical06@borisjeletich.nl>
2021-04-11 21:43:26 +0200

    Co-authored-by: Boricraft-Development <Boricraft-Development@users.noreply.github.com>

---
+++ subpages/subjs/login.js
@@ -0,0 +1,8 @@
+function mouseoverPass(obj) {
+    var obj = document.getElementById('InputPassword1');
+    obj.type = "text";
+  }
+  function mouseoutPass(obj) {
+    var obj = document.getElementById('InputPassword1');
+    obj.type = "password";
+  }
