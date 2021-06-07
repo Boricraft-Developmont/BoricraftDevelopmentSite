cba1ce5495338bd24becffc3504cb21ac28ebe4a
Radical06 <radical06@borisjeletich.nl>
2021-06-07 23:56:05 +0200

    Initial commit

--- subpages/subjs/login.js
+++
@@ -1,8 +0,0 @@
-function mouseoverPass(obj) {
-    var obj = document.getElementById('InputPassword1');
-    obj.type = "text";
-  }
-  function mouseoutPass(obj) {
-    var obj = document.getElementById('InputPassword1');
-    obj.type = "password";
-  }
