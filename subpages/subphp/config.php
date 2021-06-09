bf2d80292d1f9d4d33372d17aec6a75f306fe65d
Boricraft-Development <48557560+Boricraft-Development@users.noreply.github.com>
2021-06-09 18:29:06 +0200

    Add files via upload

--- subpages/subphp/config.php
+++ subpages/subphp/config.php
@@ -1,1 +1,16 @@
-
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'WowSuchPlaceholder');
+define('DB_PASSWORD', 'MuchFake');
+define('DB_NAME', 'DogeVeryHappyToday');
+ 
+/* Attempt to connect to MySQL database */
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($db === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
