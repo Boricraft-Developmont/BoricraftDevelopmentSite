3311dbfb37749c0bc8c718df56bb15576e7a5e13
Boricraft-Development <boris.jeletich@outlook.com>
2021-11-05 19:26:10 +0100

    💫 Added animated background for register page

--- registration/register.php
+++ registration/register.php
@@ -2,38 +2,50 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Registration system PHP and MySQL</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
+	<title>Registration</title>
+	<link rel="stylesheet" href="style.css">
+	<script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
+
+
 </head>
 <body>
-  <div class="header">
-  	<h2>Register</h2>
-  </div>
+<div id="particles-js">
+  	<div class="header">
+  		<h2>Register</h2>
+  	</div>
+
 	
-  <form method="post" action="register.php">
-  	<?php include('errors.php'); ?>
-  	<div class="input-group">
-  	  <label>Username</label>
-  	  <input type="text" name="username" value="<?php echo $username; ?>">
-  	</div>
-  	<div class="input-group">
-  	  <label>Email</label>
-  	  <input type="email" name="email" value="<?php echo $email; ?>">
-  	</div>
-  	<div class="input-group">
-  	  <label>Password</label>
-  	  <input type="password" name="password_1">
-  	</div>
-  	<div class="input-group">
-  	  <label>Confirm password</label>
-  	  <input type="password" name="password_2">
-  	</div>
-  	<div class="input-group">
-  	  <button type="submit" class="btn" name="reg_user">Register</button>
-  	</div>
-  	<p>
-  		Already a member? <a href="login.php">Sign in</a>
-  	</p>
-  </form>
+  	<form id="regform" method="post" action="register.php">
+  		<?php include('errors.php'); ?>
+  		<div class="input-group">
+  		  <label>Username</label>
+  		  <input type="text" name="username" value="<?php echo $username; ?>">
+  		</div>
+  		<div class="input-group">
+  		  <label>Email</label>
+  		  <input type="email" name="email" value="<?php echo $email; ?>">
+  		</div>
+  		<div class="input-group">
+  		  <label>Password</label>
+  		  <input type="password" name="password_1">
+  		</div>
+  		<div class="input-group">
+  		  <label>Confirm password</label>
+  		  <input type="password" name="password_2">
+  		</div>
+  		<div class="input-group">
+  		  <button type="submit" class="btn" name="reg_user">Register</button>
+  		</div>
+  		<p>
+  			Already a member? <a href="login.php">Sign in</a>
+  		</p>
+  	</form>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
+  <script>
+	particlesJS.load('particles-js', 'particles.json', function() {
+	  console.log('callback - particles.js config loaded');
+	});
+  </script>
 </body>
 </html>
